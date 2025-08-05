1929b2f5a98e30d2c31ba358ee920c159d1b3e6b
AlejoUzca <122064813+AlejoUzca@users.noreply.github.com>
2025-08-05 15:12:50 -0400

    Update Metroart.py

--- Metroart.py
+++ Metroart.py
@@ -1,1 +1,7 @@
+import requests
+from PIL import Image
 
+from repositorio import obtener_departamentos, obtener_objeto_por_id, buscar_objetos, get_url_nacionalidades, obtener_contenido_nacionalidades
+from Obra import Obra
+from Artista import Artista
+from Departamento import Departamento
