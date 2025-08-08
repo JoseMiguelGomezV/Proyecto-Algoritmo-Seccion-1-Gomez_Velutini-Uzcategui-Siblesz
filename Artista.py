585f2edf96a288f7b99cdb7a42c6b4b4432fe97f
Gerardo Siblesz <gsiblesz@correo.unimet.edu.ve>
2025-08-08 12:10:00 -0400

    Update Artista.py
    Artitasta listo.

--- Artista.py
+++ Artista.py
@@ -4,3 +4,9 @@ class Artista:
         self.nacionalidad = nacionalidad
         self.nacimiento = nacimiento
         self.muerte = muerte
+
+    def show_detalles(self):
+        print(f"Nombre del Artista: {self.nombre}")
+        print(f"Nacionalidad del artista: {self.nacionalidad}")
+        print(f"Fecha de nacimiento: {self.nacimiento}")
+        print(f"Fecha de muerte: {self.muerte}")
