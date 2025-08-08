fbc0187d3269906a232cf60d5cb0d09127ec05e2
Gerardo Siblesz <gsiblesz@correo.unimet.edu.ve>
2025-08-08 12:15:03 -0400

    Update Obra.py
    Listo obra.

--- Obra.py
+++ Obra.py
@@ -6,4 +6,16 @@ class Obra:
         self.clasificacion = clasificacion
         self.fecha_creacion = fecha_creacion
         self.imagen = imagen
-        self.departamento = departamento  
+        self.departamento = departamento
+
+    def show_resumen(self):
+        """Muestra una versión corta de la información de la obra, para listas."""
+        print(f"ID: {self.id_obra}, Titulo: {self.titulo}, Autor: {self.artista.nombre}")
+
+    def show_detalles(self):
+        """Muestra toda la información detallada de la obra."""
+        print(f"\nTítulo: {self.titulo}")
+        self.artista.show_detalles()
+        print(f"Tipo: {self.clasificacion}")
+        print(f"Año de creación: {self.fecha_creacion}")
+        print(f"Departamento: {self.departamento.nombre}")
