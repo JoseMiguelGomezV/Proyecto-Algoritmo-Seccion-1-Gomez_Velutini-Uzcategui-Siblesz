3544bd1e61fd66eaedd70a7aea63d797a78aac0a
Gerardo Siblesz <gsiblesz@correo.unimet.edu.ve>
2025-08-08 12:13:18 -0400

    Update Departamento.py
    Listo departamentos.

--- Departamento.py
+++ Departamento.py
@@ -2,3 +2,7 @@ class Departamento:
     def __init__(self, id_departamento, nombre):
         self.id_departamento = id_departamento
         self.nombre = nombre
+
+    def show(self):
+        print(f"{self.id_departamento}: {self.nombre}")
+        
